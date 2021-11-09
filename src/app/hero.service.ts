9b63813f392fc3f08c5685a72cf30bb1b465f446
Kevin Teos <kevin.teos@techelevator.com>
2021-11-08 22:35:14 -0500

    added error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { Heroes } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service'
 import { HttpClient, HttpHeaders } from '@angular/common/http'; // this is to make http request and headers
+import { catchError, map , tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -26,13 +27,28 @@ export class HeroService {
   //   // this.log('Fetched Heroes'); this is the previous code 
   //   return heroes;
 
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError<Hero[]>('getHeroes',[]))
+    );
    }
 
   getHero( id: number): Observable<Hero> {
     const hero = Heroes.find( h => h.id === id)!; //im not really too sure what the exclamation point is doing
     this.log(`fetched hero id: ${id}`);
     return of(hero);
+  }
+
+/** this handles error
+ * it will let the app keep running 
+ * @param operation - name of the operation that failed 
+ * @param result - optional value to return as the observable result 
+ */
+  private handleError<T>(operation = 'operation', result?:T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // this logs the error out to the console
+      this.log(`${operation} failed: ${error.message}`);// this prints out using our message service 
+      return of(result as T);// this returns an empty result of whatever type we are working with 
+    }
   }
 
 
